2bf02dbd725ffb4e7d1674efc8c81dc14ec45800
Abdul Basith <developer.basith@gmail.com>
2022-04-11 10:17:20 +0100

    Util classes added

--- app/src/main/java/com/stackexchange/util/NetworkInterceptor.kt
+++ app/src/main/java/com/stackexchange/util/NetworkInterceptor.kt
@@ -1,4 +1,12 @@
 package com.stackexchange.util
 
-class NetworkInterceptor {
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.util.concurrent.TimeUnit
+
+class NetworkInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        return chain.proceed(chain.withReadTimeout(4, TimeUnit.SECONDS).request())
+    }
 }
+
