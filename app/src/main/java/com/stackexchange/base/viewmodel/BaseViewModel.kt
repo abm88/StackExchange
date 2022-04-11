465d13e5a862918baa0240a2523103c91a77fe83
Abdul Basith <developer.basith@gmail.com>
2022-04-11 11:42:09 +0100

    Generic viewModels added

--- app/src/main/java/com/stackexchange/base/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/stackexchange/base/viewmodel/BaseViewModel.kt
@@ -1,4 +1,26 @@
 package com.stackexchange.base.viewmodel
 
-class BaseViewModel {
+import androidx.lifecycle.ViewModel
+import com.stackexchange.base.BaseState
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+abstract class BaseViewModel<STATE : BaseState>(
+    val initialState: STATE
+) : ViewModel() {
+
+    abstract val state: SingleLiveEvent<STATE>
+
+    private val compositeDisposable by lazy {
+        CompositeDisposable()
+    }
+
+    fun add(subject: Disposable) {
+        compositeDisposable.add(subject)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 }
