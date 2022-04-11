0f4632e95b3d0108f5714cbe35154b9580819c84
Abdul Basith <developer.basith@gmail.com>
2022-04-11 10:04:58 +0100

    app class updated

--- app/src/main/java/com/stackexchange/StackExchangeApplication.kt
+++ app/src/main/java/com/stackexchange/StackExchangeApplication.kt
@@ -1,4 +1,16 @@
 package com.stackexchange
 
-class StackExchangeApplication {
+import com.stackexchange.di.AppComponent
+import com.stackexchange.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+class StackExchangeApplication : DaggerApplication(){
+    private val appComponent: AppComponent by lazy {
+        DaggerAppComponent.builder()
+            .application(this)
+            .build()
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> = appComponent
 }
