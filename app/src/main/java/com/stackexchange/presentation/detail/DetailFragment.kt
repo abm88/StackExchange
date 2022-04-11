16a53ef1db365c2ab6afc2515fcf80267e5e55f2
Abdul Basith <developer.basith@gmail.com>
2022-04-11 15:28:20 +0100

    PresentationLayer: Details Fragment implemented

--- app/src/main/java/com/stackexchange/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/stackexchange/presentation/detail/DetailFragment.kt
@@ -1,4 +1,36 @@
 package com.stackexchange.presentation.detail
 
-class DetailFragment {
+import android.content.Intent
+import android.net.Uri
+import com.stackexchange.R
+import com.stackexchange.base.ui.ViewModelErrorSuccessFragment
+import com.stackexchange.domain.model.StackExchangeUserEntity
+import com.stackexchange.presentation.main.StackExchangeViewModel
+import com.stackexchange.presentation.stack_exchange.StackExchangeEvent
+import com.stackexchange.presentation.stack_exchange.StackExchangeState
+import kotlinx.android.synthetic.main.detail_screen.*
+
+
+class DetailFragment: ViewModelErrorSuccessFragment<StackExchangeState, StackExchangeEvent, StackExchangeViewModel>(){
+    override val contentResourceId: Int = R.layout.detail_screen
+
+    override fun getViewModelClass(): Class<StackExchangeViewModel> = StackExchangeViewModel::class.java
+
+    override fun initView() {
+        arguments?.getParcelable<StackExchangeUserEntity>("user_detail")?.apply {
+            detailTitleTextView.text = userName
+            detailReputationTextView.text = getString(R.string.reputation, reputation)
+            if(topTags.isNullOrEmpty()){
+                detailTagTextView.text = getString(R.string.no_tags)
+            }else {
+                detailTagTextView.text = getString(R.string.tags, topTags)
+            }
+            detailLinkTextView.text = link
+            detailLinkTextView.setOnClickListener {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(link)))
+            }
+            imageLoader.loadImage(
+                detailImageView, 0, 0, avatar)
+        }
+    }
 }
