c57aec05488c60774a2e0bb475e44f2b5ccbeaf9
Abdul Basith <developer.basith@gmail.com>
2022-04-11 11:28:22 +0100

    BaseState.kt implemented to handle app states

--- app/src/main/java/com/stackexchange/base/BaseState.kt
+++ app/src/main/java/com/stackexchange/base/BaseState.kt
@@ -1,4 +1,44 @@
 package com.stackexchange.base
 
-class BaseState {
+import com.stackexchange.error.ErrorEntity
+
+data class BaseState(
+    var error: ErrorEntity = ErrorEntity.NoError,
+    var loading: Boolean = true,
+) {
+
+    fun noError() = this.copy(
+        error = ErrorEntity.NoError,
+    )
+
+    fun noErrorNoLoading() = this.copy(
+        error = ErrorEntity.NoError,
+        loading = false,
+    )
+
+    fun onErrorNoLoading(failure: ErrorEntity) = this.copy(
+        error  = ErrorEntity.NotFound(""),
+        loading = false,
+    )
+
+    fun loading() = this.copy(
+        error = ErrorEntity.NoError,
+        loading = true,
+    )
+
 }
+
+
+
+abstract class ViewModelState {
+    open var baseState: BaseState = BaseState()
+}
+
+fun <T : ViewModelState> T.loading(): T = with(this) {
+    this.baseState.loading = true
+    this
+}
+
+interface BaseEvent
+
+
